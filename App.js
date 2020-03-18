1e03f78569e2632ef149d91bbc547c4ed455d6f6
1997808 <khanhhuyle22@gmail.com>
2020-03-18 11:42:17 +0700

    done basic drawer navigation, new dummy screen

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import IndexTabNavigator from './navigation/IndexNavigation'
+import IndexDrawerNavigator from './navigation/IndexNavigation'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <IndexTabNavigator />
+      <IndexDrawerNavigator />
     </View>
   );
 }
