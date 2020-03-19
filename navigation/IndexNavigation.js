6ef2ed28f3e49f633de2efec2abd35da86c2b1d7
thang1405 <43245805+thang1405@users.noreply.github.com>
2020-03-19 16:53:08 +0700

    add messages screen

--- navigation/IndexNavigation.js
+++ navigation/IndexNavigation.js
@@ -1,4 +1,6 @@
-import { Platform } from 'react-native'
+import React from 'react'
+import { View, Text, StyleSheet, Image, Platform, ScrollView } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
 
 import IndexScreen from '../screens/IndexScreen'
 import ProfileScreen from '../screens/ProfileScreen'
@@ -11,43 +13,139 @@ import NotificationScreen from '../screens/DrawerScreen/NotificationScreen'
 import SettingScreen from '../screens/DrawerScreen/SettingScreen'
 import PasswordScreen from '../screens/DrawerScreen/PasswordScreen'
 
-import { createAppContainer } from 'react-navigation'
+import { createAppContainer, SafeAreaView } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
-import { createDrawerNavigator } from 'react-navigation-drawer'
-
-// const IndexNavigator = createStackNavigator({
-//   Index: IndexScreen,
-//   Profile: ProfileScreen,
-// }, {
-// defaultNavigationOptions: {
-//   headerStyle: {
-//     backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
-//   },
-//   headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
-// }
-// })
+import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer'
+import Color from '../constants/Color'
 
 const IndexTabNavigator = createBottomTabNavigator({
-  Index: IndexScreen,
-  Note: NoteScreen,
-  Calendar: CalendarScreen,
-  Message: MessageScreen,
-  Payment: PaymentScreen,
+  Index: {
+    screen: IndexScreen,
+    navigationOptions: {
+      tabBarIcon: ({ tintColor }) => {
+        return <Ionicons name={'md-home'} size={23} color={tintColor} />;
+      },
+    },
+  },
+
+  Note: {
+    screen: NoteScreen,
+    navigationOptions: {
+      tabBarIcon: ({ tintColor }) => {
+        return <Ionicons name={'md-list-box'} size={23} color={tintColor} />;
+      },
+    },
+  },
+
+  Calendar: {
+    screen: CalendarScreen,
+    navigationOptions: {
+      tabBarIcon: ({ tintColor }) => {
+        return <Ionicons name={'md-calendar'} size={23} color={tintColor} />;
+      },
+    },
+  },
+
+  Message: {
+    screen: MessageScreen,
+    navigationOptions: {
+      tabBarIcon: ({ tintColor }) => {
+        return <Ionicons name={'md-text'} size={23} color={tintColor} />;
+      },
+    },
+  },
+
+  Payment: {
+    screen: PaymentScreen,
+    navigationOptions: {
+      tabBarIcon: ({ tintColor }) => {
+        return <Ionicons name={'md-wallet'} size={23} color={tintColor} />;
+      },
+    },
+  },
+}, {
+  tabBarOptions: {
+    activeTintColor: Color.blue,
+    inactiveTintColor: Color.gray,
+  }
 })
 
 const IndexDrawerNavigator = createDrawerNavigator({
   Index: IndexTabNavigator,
+  Profile: ProfileScreen,
   Notification: NotificationScreen,
   Setting: SettingScreen,
   Password: PasswordScreen,
-  Payment: PaymentScreen,
-  Profile: ProfileScreen,
+  // Payment: PaymentScreen,
+  // demo more screen
+}, {
+  contentComponent: (props) => (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.userContainer}>
+        {/* <Image
+          style={{ width: 100, height: 100 }}
+          source={require('../assets/icon/avatar-2.png')}
+        /> */}
+        <Image
+          style={styles.image}
+          source={{ uri: 'https://image.shutterstock.com/image-photo/portrait-smiling-red-haired-millennial-260nw-1194497251.jpg' }}
+        />
+        <View>
+          <View style={styles.infoContainer}>
+            <Ionicons name={'md-person'} size={23} color={Color.blue} />
+            <Text style={styles.userInfo}>Ngọc Kenny</Text>
+          </View>
+
+          <View style={styles.infoContainer}>
+            <Ionicons name={'md-call'} size={23} color={Color.blue} />
+            <Text style={styles.userInfo}>0293928398238</Text>
+          </View>
+        </View>
+      </View>
+      <ScrollView>
+        <DrawerItems {...props} />
+      </ScrollView>
+    </SafeAreaView>
+  )
 })
 
 // const IndexNavigator = createStackNavigator({
 //   IndexDraw: IndexDrawerNavigator,
 //   IndexNav: IndexTabNavigator,
 // })
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: 150
+  },
+
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    borderColor: Color.gray,
+    borderWidth: 3
+  },
+
+  userContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+
+  userInfo: {
+    fontSize: 16,
+    padding: 5,
+    color: Color.blue
+  },
+
+  infoContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+})
 
 export default createAppContainer(IndexDrawerNavigator)
--- screens/MessageScreen.js
+++ screens/MessageScreen.js
@@ -1,21 +1,65 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet,FlatList, Alert, Platform } from 'react-native'
+import MessageListItem from '../components/messageComponents/MessageListItem'
 
-const MessageScreen = props => {
-  return (
-    <View style={styles.container}>
-      <Text>The MessageScreen page</Text>
-    </View>
-  )
+export default class Messages extends React.Component{
+  
+  
+  constructor(props){
+    super(props);
+    this.state={
+      messages:[
+        {
+          id:1,
+          title:'Thực đơn',
+          content:'',
+          sender:'Vũ Minh Hải',
+          date:'11:11 AM,05/03/2020'
+        },
+        {
+          id:2,
+          title:'Chơi và học',
+          content:'',
+          sender:'Vũ Minh Hải',
+          date:'11:11 AM,02/03/2020'
+        }
+
+      ]
+    };
+  }
+
+  
+  render(){
+    const {messageList}=this.state.messages;
+    return(
+      <View style={styles.container}>
+        <FlatList
+        data={messageList}
+        renderItem={({ item})=>
+          <MessageListItem
+            message={item}
+           
+            onPress={()=>{
+              Alert.alert("Pressed");
+            }}
+          ></MessageListItem>
+        }
+        keyExtractor={item => `${item.id}`}
+        ></FlatList>
+      </View>
+    );
+  }
+
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+
+    marginTop: 20,
   }
 })
 
 
-export default MessageScreen
