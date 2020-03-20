bff0d4388198d925d647f9b5d7d5d81c94700d13
1997808 <khanhhuyle22@gmail.com>
2020-03-20 10:07:22 +0700

    new stack

--- navigation/StackIndexNavigator.js
+++ navigation/StackIndexNavigator.js
@@ -4,11 +4,20 @@ import { View, Text, StyleSheet, Image, Platform, ScrollView } from 'react-nativ
 import { Ionicons } from '@expo/vector-icons'
 
 import IndexScreen from '../screens/IndexScreen'
-import AttendanceScreen from '../screens/AttendanceScreen'
-import DiaryScreen from '../screens/DiaryScreen'
-import HealthScreen from '../screens/HealthScreen'
-import ImgVideoScreen from '../screens/ImgVideoScreen'
-import LunchScreen from '../screens/LunchScreen'
+import AttendanceScreen from '../screens/IndexScreen/AttendanceScreen'
+import DiaryScreen from '../screens/IndexScreen/DiaryScreen'
+import HealthScreen from '../screens/IndexScreen/HealthScreen'
+import ImgVideoScreen from '../screens/IndexScreen/ImgVideoScreen'
+import LunchScreen from '../screens/IndexScreen/LunchScreen'
 
 import { createStackNavigator } from 'react-navigation-stack'
 import Color from '../constants/Color'
+
+const IndexStackNavigator = createStackNavigator({
+  Index: IndexScreen,
+  Attendance: AttendanceScreen,
+  Diary: DiaryScreen,
+  Health: HealthScreen,
+  ImgVideo: ImgVideoScreen,
+  Lunch: LunchScreen,
+})
